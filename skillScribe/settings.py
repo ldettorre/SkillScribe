a8e5241a618c7dfd3697f0db127b17136719a3f3
Luca D'Ettorre <ldettorre21@gmail.com>
2023-06-29 21:53:46 +0100

    Deployment: Bug persisted. Updated the url within allowed hosts from the settings.py file.

--- skillScribe/settings.py
+++ skillScribe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 
-ALLOWED_HOSTS = ['https://skillscribe-d47e2a842c2b.herokuapp.com']
+ALLOWED_HOSTS = ['skillscribe-d47e2a842c2b.herokuapp.com',]
 
 
 # Application definition
