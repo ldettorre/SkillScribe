61e696c2a0fac186b971669f7f9383c8d7cfd62a
Luca D'Ettorre <ldettorre21@gmail.com>
2023-07-09 20:09:06 +0100

    Added a basic html form to the login and registration html templates. Added login, logout and registration logic to the relevant function views in users app. Opted to use the build in django user model as it allowed easier set up and may implement a custom version to add fields. Input validation and sanitisation will be reviwed also at a later stage.

--- skillScribe/urls.py
+++ skillScribe/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("home.urls")),
     path('questions/', include("entries.urls")),
-    path('users/', include("users.urls")),
+    path('', include("users.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,67 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+# login and logout are given aliases as they conflict with the view names
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            print("User logged in.")
+            return redirect('profile')
+        else:
+            print("User not logged in.")
+            return redirect('login')
+    else:
+        return render(request, 'users/login.html')
+
+def logout(request): 
+    auth_logout(request)
+    print('User logged out')
+    return redirect('login')
+
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        print('Registration Attempt')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        
+        if password1 == password2:
+            print('Passwords match')
+            # Check if the username is already taken
+            if User.objects.filter(username=username).exists():
+                print("Username already taken.")
+                return redirect('register')
+            # Check if the email is already taken
+            elif User.objects.filter(email=email).exists():
+                print("Email already registered.")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name=first_name, 
+                last_name=last_name, 
+                username=username, email=email,
+                password=password1)
+                user.save()
+                print("User registered.")
+                auth_login(request,user)
+                print("User now logged in after new registration")
+                return redirect('profile')
+                
+        else:
+            print('Passwords dont match')
+            return redirect('/')
+    else:
+        return render(request, 'users/register.html')
+
 
 def profile(request):
     return render(request, 'users/profile.html')
