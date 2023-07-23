fef907b65059776a8f734800aa1a46ab9e0ef003
Luca D'Ettorre <ldettorre21@gmail.com>
2023-07-23 21:30:18 +0100

    Basic styling added to browse questions page. Added a js file and logic to detect window resolution so to hide or show the appropriate category selection elements based on screen width

---
+++ static/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    function showCategorySelectElement() {
+        if ($(window).width() <= 768) {
+            document.getElementById('category-column').hidden = true
+            document.getElementById('category-dropdown').hidden = false
+        }
+        else{
+            document.getElementById('category-column').hidden = false
+            document.getElementById('category-dropdown').hidden = true
+            
+        }
+    }
+
+    showCategorySelectElement();
+
+    $(window).resize(function() {
+        console.log("Changing width detected...")
+        showCategorySelectElement();
+    });
+});
