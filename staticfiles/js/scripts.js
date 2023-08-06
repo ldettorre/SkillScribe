6bb6a31bd24bde05fef58398ce40a1fc1f0e2314
Luca D'Ettorre <ldettorre21@gmail.com>
2023-08-06 22:25:15 +0100

    Updated the category-dropdown element on the questions page as it was not contributing to a good UI experiece on mobile. Relevant code has been commented out temporarily while it is reviewed

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
     
+    // Commented out the category-dropdown related code as it is currently under
+    // review for a better UI experience.
     function showCategorySelectElement() {
         if ($(window).width() <= 768) {
             document.getElementById('category-column').hidden = true
-            document.getElementById('category-dropdown').hidden = false
+            // document.getElementById('category-dropdown').hidden = false
         }
         else{
             document.getElementById('category-column').hidden = false
-            document.getElementById('category-dropdown').hidden = true
+        //     document.getElementById('category-dropdown').hidden = true
         }
     }  
 
-    $('#dropdown-selection').change(function() {
-        var selection = $(this).val(); 
-        var selectedCategory = document.getElementById(selection);
-        selectedCategory.scrollIntoView();         
-    });
+    // $('#dropdown-selection').change(function() {
+    //     var selection = $(this).val(); 
+    //     var selectedCategory = document.getElementById(selection);
+    //     selectedCategory.scrollIntoView();
+    // });
 
 
     showCategorySelectElement();
